52f0ce9d9cab9eb62cb321519564282528d6ce54
James Cosgrove <jamescosgrove2000@live.co.uk>
2020-04-24 18:35:40 +0100

    Initial Commit

---
+++ ImageLoadingView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ImageLoadingView
+//
+//  Created by James Cosgrove on 24/04/2020.
+//  Copyright © 2020 James Cosgrove. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
